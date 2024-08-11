8a6e5159678b401f0bc9850a35c0e8b52b890d44
cvillegas5 <cvillegas5@miners.utep.edu>
2024-08-10 23:40:01 -0600

    server and client added

---
+++ client.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+
+import socket, sys, re, os, time
+sys.path.append("../lib")     # for params
+import params
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+paramMap = params.parseParams(switchesVarDefaults)
+server, usage = paramMap["server"], paramMap["usage"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af,socktype,proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error : %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+while True:
+    files = input("Input Files to send [to stop enter 'stop': ")
+    files = files.split()
+    if 'stop' in files:
+        break
+    archive = b''
+    for file in files:
+        curFile = os.open(file, os.O_RDONLY)
+        reader = buf.BufferedFdReader(curFile)
+        fileSize = os.path.getsize(file)
+
+        header = bytearray(64)
+        for i in range(len(file)):
+            header[i] = file[i].encode()[0]
+        for i in range(len(str(fileSize))):
+            header[i+32] = str(fileSize)[i].encode()[0]
+        data = reader.readByte()
+        content = []
+        while data is not None:
+            content.append(data)
+            data = reader.readByte()
+        reader.close()
+
+        fileCont = header + bytearray(content)
+        archive  += fileCont
+    s.send(archive)
+s.shutdown(socket.SHUT_WR)
+s.close()
---
+++ server.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env pythonp3
+
+import socket, sys, re, os, time
+sys.path.append("../lib")     # for params
+import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort'), 'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''         # Symbolic name meaning all available interfaces
+
+pidAddr = {}            # for active connections: maps pid-> client addr
+
+if paramMap['usage']:
+    params.usage()
+
+# server code to be run by child
+def interactWClient(connAddr):
+    sock, addr = connAddr
+    print(f'Child: pid = {os.getpid()} connected to client at {addr}')
+    content = b''
+
+    while len(contents):
+        header = contents[:64]
+        fileName = header[:32].decode().strip('\x00')
+        fileName = f'src/{fileName}'
+
+        fileSize = header[32:64].decode().strip('\x00')
+        fileSize = int(fileSize)
+
+        fileData = contents[64:64+fileSize]
+        contents = contents[64+fileSize:]
+
+        fd = os.open(fileName, os.O_WRONLY | os.O_CREAT)
+        writer = buf.BufferedFdWriter(fd)
+        for i in fileData:
+            writer.writeByte(i)
+        writer.close()
+    sock.shutdown(socket.SHUT_WR)
+    sys.exit(0)     # terminate child
+
+listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# listener socket will unbind immediately on close
+listenSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# accept will block for no more than 5s
+listenSock.settimeout(5)
+# bind listener socket to port
+listenSock.vind((listenAddr, listenPort))
+#set state to listen
+listenSock.listen(1)           # allow only one outstanding request
+
+# s is a factory for connected sockets
+
+
+while True:
+    # reap zombie children (if  any)
+    while pidAddr.keys():
+        # Check for exited children(zombies). If none, don't block (hang)
+        if (waitResult := os.waitid(os.P_ALL, 0, os.WNOHANG | os.WEXITED)):
+            zPid, zStatus = waitResult.si_pid, waitResult.si_status
+            print(f"""zombie reaped:
+            \tpid={zPid}, status={zStatus}
+            \twas connected to {pidAddr[zPid]}""")
+            del pidAddr[zPid]
+        else:
+            break      # no zombies; break from loop
+    print(f"Currently {len(pidAddr.keys())} clients")
+
+    try:
+        connSockAddr = listenSock.accept() # accept connection from a new client
+    except TimeoutError:
+        connSockAddr = None
+
+    if connSockAddr is None:
+        continue
+
+    forkResult = os.fork()   # fork child for this client
+    if(forkResult == 0):      # child
+        listenSock.close()     # child doesn't need listenSock
+        interactWClient(connSockAddr)
+    # parent
+    sock, addr = connSockAddr
+    sock.close()   # parent closes its connection to the client
+    pidAddr[forkResult] = addr
+    print(f"spawned off child with pid = {forkResult} at addr {addr}")
